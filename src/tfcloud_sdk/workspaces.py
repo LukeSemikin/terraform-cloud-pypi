d013ddd6751634a0f03611b2b0064dbff7a0923d
LukeSemikin <lukesemikin@gmail.com>
2024-06-08 20:00:22 +1000

    Initial Release

---
+++ src/tfcloud_sdk/workspaces.py
@@ -0,0 +1,245 @@
+from json import dumps, loads
+from .authentication import Authentication
+
+class Workspaces:
+
+    def __init__(self, organisation, access_token):
+        self.organisation = organisation
+        self.base_url = "https://app.terraform.io/api/v2/"
+        self.auth = Authentication(organisation=organisation, access_token=access_token)
+        self.tf_session = self.auth.get_session()
+
+    def workspace_labels(self):
+        vcs_labels = [
+            "branch",
+            "identifier",
+            "ingress-submodules",
+            "oauth-token-id",
+            "tags-regex"
+        ]
+        settings_labels = [
+            "execution-mode"
+        ]
+        data_labels = [
+            "type",
+            "id"
+        ]
+        attributes_labels = [
+            "agent-pool-id",
+            "allow-destroy-plan",
+            "agent-pool-id",
+            "auto-apply",
+            "auto-apply-run-trigger",
+            "auto-destroy-at",
+            "auto-destroy-activity-duration",
+            "description",
+            "execution-mode",
+            "file-triggers-enabled",
+            "global-remote-state",
+            "operation",
+            "queue-all-runs",
+            "setting-overwrites",
+            "source-name",
+            "source-url",
+            "speculative-enabled",
+            "terraform-version",
+            "trigger-patterns",
+            "trigger-prefixes",
+            "working-directory"
+        ]
+        return vcs_labels, settings_labels, data_labels, attributes_labels
+    
+
+    def workspace_validation_set(self, kwargs):
+        vcs_labels, settings_labels, data_labels, attributes_labels = self.workspace_labels()
+        all_labels = vcs_labels + settings_labels + data_labels + attributes_labels
+        for key, value in args:
+            if key not in all_labels:
+                print(f"Key: {key} not recognised")
+                exit(125)
+
+    def form_data_set(self, name, args):
+        vcs_labels, settings_labels, data_labels, attributes_labels = self.workspace_labels()
+        attributes = {
+            "name": name
+        }
+        data = {}
+        settings = {}
+        vcs = {}
+        for key, value in args:
+            if key in attributes_labels:
+                attributes.update(key, value)
+            elif key in data_labels:
+                data.update(key, value)
+            elif key in vcs_labels:
+                vcs.update(key, value)
+            elif key in settings_labels:
+                settings.update(key, value)
+        if settings is not None: 
+            attributes.update({"setting-overwrites": settings})
+        if vcs is not None: 
+            attributes.update({"vcs-repo": vcs})
+        payload = {
+            "data": {
+                "type": "workspace",
+                "attributes": attributes
+            }
+        }
+        if data is not None:
+            payload.update({"relationships": { "projects": { "data": data } }})
+        return dumps(payload)
+    
+    def form_tags_dataset(self, new_tags):
+        tags = {}
+        for value in new_tags:
+            tags.update({
+                "type": "tags",
+                "attributes": {
+                    "name": value
+                }
+            })
+        payload = {
+            "data": [
+                tags
+            ]
+        }
+        return dumps(payload)
+
+#Get Requests
+
+    def list_workspaces(self):
+        workspaces = []
+        url = f"{self.base_url}organizations/{self.organisation}/workspaces"
+        response = self.tf_session.get(url=url)
+        for page in range (0, loads(response.content)['meta']['pagination']['total-pages']):
+            data = loads(self.tf_session.get(url=url,params={'page':page}).content)['data']
+            for workspace in data:
+                workspaces.append(workspace)
+        return response.status_code, workspaces
+    
+    def show_workspace(self, workspace_name):
+        url = f"{self.base_url}organizations/{self.organisation}/workspaces/{workspace_name}"
+        return self.tf_session.get(url=url)
+    
+    def get_remote_state_consumers(self, workspace_id):
+        url = f"{self.base_url}/workspaces/{workspace_id}/relationships/remote-state-consumers"
+        return self.tf_session.get(url=url)
+    
+    def get_workspace_tags(self, workspace_id):
+        url = f"{self.base_url}workspaces/{workspace_id}/relationships/tags"
+        return self.tf_session.get(url=url)
+    
+#Post Requests
+    
+    def create_workspace(self, name, kwargs):
+        self.workspace_validation_set()
+        payload = self.form_data_set(name, kwargs)
+        url = f"{self.base_url}organizations/{self.organisation}/workspaces"
+        return self.tf_session.post(data=payload, url=url)
+
+    def safe_delete_workspace(self, workspace_id=None, workspace_name=None):
+        if workspace_id is not None:
+            url = f"{self.base_url}workspaces/{workspace_id}/actions/safe-delete"
+        elif workspace_name is not None:
+            url = f"{self.base_url}organizations/{self.organisation}/workspaces/{workspace_name}/actions/safe-delete"
+        else:
+            print("Either a Workspace ID or Workspace Name must be provided")
+            return None
+        return self.tf_session.post(url=url)
+    
+    def lock_workspace(self, workspace_id, reason):
+        url = f"{self.base_url}workspaces/{workspace_id}/actions/lock"
+        payload = dumps({
+            "reason": reason
+        })
+        return self.tf_session.post(url=url, data=payload)
+
+    def unlock_workspace(self, workspace_id):
+        url = f"{self.base_url}workspaces/{workspace_id}/actions/unlock"
+        return self.tf_session.post(url=url)
+
+    def force_unlock_workspace(self, workspace_id):
+        url = f"{self.base_url}workspaces/{workspace_id}/actions/force-unlock"
+        return self.tf_session.post(url=url)
+
+    def add_remote_state_consumers(self, workspace_id, remote_consumer):
+        payload = dumps({
+            "data" : [
+                {
+                    "id": remote_consumer,
+                    "type": "workspaces"
+                }
+            ]
+        })
+        url = f"{self.base_url}/workspaces/{workspace_id}/relationships/remote-state-consumers"
+        return self.tf_session.post(url=url, data=payload)
+
+    def add_tags(self, workspace_id, tags):
+        payload = self.form_tags_dataset(tags)
+        url = f"{self.base_url}workspaces/{workspace_id}/relationships/tags"
+        return self.tf_session.post(url=url, data=payload)
+
+#Patch Requests
+    
+    def update_workspace(self, name, **kwargs):
+        self.workspace_validation_set()
+        payload = self.form_data_set(name, **kwargs)
+        url = f"{self.base_url}organizations/{self.organisation}/workspaces"
+        return self.tf_session.patch(data=payload, url=url)
+
+    def assign_ssh_key(self, workspace_id, key_id):
+        payload = dumps({
+            "data": {
+                "attributes": {
+                    "id": key_id
+                },
+                "type": "workspaces"
+            }
+        })
+        url = f"{self.base_url}/workspaces/{workspace_id}/relationships/ssh-key"
+        return self.tf_session.patch(url=url, data=payload)
+
+    def unassign_ssh_key(self, workspace_id):
+        return self.assign_ssh_key(workspace_id=workspace_id, key_id="null")
+
+    def update_remote_state_consumers(self, workspace_id, remote_consumer):
+        payload = dumps({
+            "data" : [
+                {
+                    "id": remote_consumer,
+                    "type": "workspaces"
+                }
+            ]
+        })
+        url = f"{self.base_url}/workspaces/{workspace_id}/relationships/remote-state-consumers"
+        return self.tf_session.patch(url=url, data=payload)
+
+        
+#Delete Requests
+        
+    def force_delete_workspace(self, workspace_id=None, workspace_name=None):
+        if workspace_id is not None:
+            url = f"{self.base_url}workspaces/{workspace_id}"
+        elif workspace_name is not None:
+            url = f"{self.base_url}organizations/{self.organisation}/workspaces/{workspace_name}"
+        else: 
+            print("Either a Workspace ID or Workspace Name must be provided")
+            return None
+        return self.tf_session.delete(url=url)
+
+    def delete_remote_state_consumers(self, workspace_id, remote_consumer):
+        payload = dumps({
+            "data" : [
+                {
+                    "id": remote_consumer,
+                    "type": "workspaces"
+                }
+            ]
+        })
+        url = f"{self.base_url}/workspaces/{workspace_id}/relationships/remote-state-consumers"
+        return self.tf_session.delete(url=url, data=payload)
+
+    def delete_workspace_tags(self, workspace_id, tags):
+        payload = self.form_tags_dataset(tags)
+        url = f"{self.base_url}workspaces/{workspace_id}/relationships/tags"
+        return self.tf_session.delete(url=url, data=payload)
---
+++ tests/test_workspaces.py
@@ -0,0 +1,92 @@
+from src.terraform_cloud.workspaces import Workspaces
+from os import environ
+from unittest import main, TestCase, skip
+from json import loads
+
+
+class TestWorkspaces(TestCase):
+
+    runner = Workspaces("luke_semikin", environ['tf_access_token'])
+    test_workspace_name = "dummy_workspace"
+    test_tags = ["test_tag"]
+    test_ssh_key = "dummy_key"
+
+    def setUp(self):
+        self.runner.create_workspace(f"{self.test_workspace_name}-safe-delete")
+        self.runner.create_workspace(f"{self.test_workspace_name}-force-delete")
+        self.workspace_id = loads(self.runner.show_workspace(self.test_workspace_name).content)['data']['id']
+        self.rsc_workspace_id = loads(self.runner.show_workspace(f"{self.test_workspace_name}-rsc").content)['data']['id']
+
+    def test_create_workspace(self):
+        self.runner.create_workspace(self.test_workspace_name)
+        response = self.runner.show_workspace(self.test_workspace_name)
+        self.assertEqual(200, response.status_code)
+
+    def test_list_workspaces_response(self):
+        response, workspaces = self.runner.list_workspaces()
+        self.assertEqual(200, response)
+
+    def test_list_workspace_name(self):
+        response, workspaces = self.runner.list_workspaces()
+        for workspace in workspaces:
+            if workspace['attributes']['name'] == self.test_workspace_name:
+                break
+        self.assertEqual(workspace['attributes']['name'],self.test_workspace_name)
+
+    def test_show_workspace_response(self):
+        self.assertEqual(200, self.runner.show_workspace(self.test_workspace_name).status_code)
+
+    def test_show_workspace_name(self):
+        self.assertEqual(loads(self.runner.show_workspace(self.test_workspace_name).content)['data']['attributes']['name'],self.test_workspace_name)
+
+    def test_lock_workspace(self):
+        self.runner.lock_workspace(self.workspace_id, "Test Lock")
+        self.assertTrue(loads(self.runner.show_workspace(self.test_workspace_name).content)['data']['attributes']['locked'])
+
+    def test_unlock_workspace(self):
+        self.runner.unlock_workspace(self.workspace_id)
+        self.assertFalse(loads(self.runner.show_workspace(self.test_workspace_name).content)['data']['attributes']['locked'])
+
+    def test_get_remote_state_consumers(self):
+        self.assertEqual(self.runner.get_remote_state_consumers(self.workspace_id).status_code, 200)
+
+    def test_add_remote_state_consumers(self):
+        self.runner.create_workspace(f"{self.test_workspace_name}-rsc")
+        self.assertEqual(204, self.runner.add_remote_state_consumers(self.workspace_id, self.rsc_workspace_id).status_code)
+
+    def test_force_unlock_workspace(self):
+        self.runner.lock_workspace(self.workspace_id, "Test Lock")
+        self.runner.force_unlock_workspace(self.workspace_id)
+        self.assertFalse(loads(self.runner.show_workspace(self.test_workspace_name).content)['data']['attributes']['locked'])
+
+    def test_add_tags(self):
+        self.assertEqual(self.runner.add_tags(self.workspace_id, self.test_tags).status_code, 204)
+
+    def test_get_workspace_tags(self):
+        self.assertEqual(self.runner.get_workspace_tags(self.workspace_id).status_code, 200)
+
+    def test_safe_delete_workspace(self):
+        self.assertEqual(self.runner.safe_delete_workspace(workspace_name=f"{self.test_workspace_name}-safe-delete").status_code, 204)
+
+    def test_force_delete_workspace(self):
+        self.assertEqual(self.runner.force_delete_workspace(workspace_name=f"{self.test_workspace_name}-force-delete").status_code, 204)
+
+    def test_update_remote_state_consumers(self):
+        self.assertEqual(self.runner.update_remote_state_consumers(self.workspace_id, self.rsc_workspace_id).status_code, 204)
+
+    def test_delete_remote_state_consumers(self):
+        self.assertEqual(self.runner.delete_remote_state_consumers(self.workspace_id, self.rsc_workspace_id).status_code, 204)
+
+    def test_delete_workspace_tags(self):
+        self.assertEqual(self.runner.delete_workspace_tags(self.workspace_id, self.test_tags).status_code, 204)
+
+    @skip("Requires SSH Key Library")
+    def test_assign_ssh_key(self):
+        self.assertEqual(self.runner.assign_ssh_key(self.workspace_id, self.test_ssh_key).content, 204)
+
+    @skip("Requires SSH Key Library")
+    def test_unassign_ssh_key(self):
+        self.assertEqual(self.runner.unassign_ssh_key(self.workspace_id).status_code, 204)
+
+if __name__ == '__main__':
+    testsuite = main()
